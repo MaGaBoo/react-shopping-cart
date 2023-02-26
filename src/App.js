a4ac16c9c72c24f12c51c369b0e2ab6e5f7b4518
María <91290073+MaGaBoo@users.noreply.github.com>
2023-02-26 13:05:21 +0100

    Refactor to custom hook for filters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Footer } from './components/Footer';
+import Header from './components/Header';
+import Products from './components/Products';
+import initialProducts from './mocks/initialProducts.json';
+export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+
+const useFilters = () => {
+
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  });
+
+  const filterProducts = (products) => {
+    return products.products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  };
+  return { filterProducts, setFilters }
+};
 
 function App() {
+  const [products] = useState(initialProducts);
+  const { filters, filterProducts, setFilters } = useFilters();
+  const filteredProducts = filterProducts(products)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        changeFilters={setFilters} />
+      <Products
+        products={filteredProducts} />
+        { IS_DEVELOPMENT && <Footer filters={filters} />}
+    </>
   );
 }
 
